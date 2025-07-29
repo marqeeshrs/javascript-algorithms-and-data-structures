fd41132f53f0d4a7219a2d60b795a1b3f3576afc
Sharef Beckum-Swearington <marqeeshrs@gmail.com>
2025-07-28 17:48:59 -0700

    UPDATE script
    Began working on my first js lesson

--- lesson-projects/buildingapyramidgenerator/script.js
+++ lesson-projects/buildingapyramidgenerator/script.js
@@ -1,6 +1,25 @@
+/**
 console.log("Let's build a pyramid using JavaScript basics: ");
 console.log("    #    ");
 console.log("   ###   ");
 console.log("  #####  ");
 console.log(" ####### ");
 console.log("#########");
+*/
+
+let character = "Hello";
+/**
+console.log(character);
+character = "World";
+console.log(character);
+let secondCharacter;
+secondCharacter = "Test";
+console.log(secondCharacter);
+secondCharacter = character;
+let profession = "teacher";
+let age;
+console.log(profession);
+console.log(age);
+*/
+let count = 8;
+console.log(count +1);

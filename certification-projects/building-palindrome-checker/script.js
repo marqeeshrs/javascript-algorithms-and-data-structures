635e174f57c0a603c5594d8706ac1644b728b017
Sharef Beckum-Swearington <marqeeshrs@gmail.com>
2025-08-11 04:26:23 -0700

    UPDATE building-palindrome-checker
    Updated index with scaffold html and began working on the script

---
+++ certification-projects/building-palindrome-checker/script.js
@@ -0,0 +1,5 @@
+const textInput = document.getElementById("text-input");
+const checkBtn = document.getElementById("check-btn");
+const resultDiv = document.getElementById("result");
+
+const checkBtn = (text) => {}
